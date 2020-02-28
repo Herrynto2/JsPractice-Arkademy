4fdc98d072cc6407441db776099520969592bc7c
Herrynto2 <herryzen02@gmail.com>
2020-02-28 15:55:36 +0700

    edit some file

--- 2.Looping.js
+++ 2.Looping.js
@@ -1,29 +1,29 @@
 //For
-// for (let i = 0; i < 10; i++) {
-//console.log(hasil);
-// }
+for (let i = 0; i < 10; i++) {
+    console.log(hasil);
+}
 
 
-// for of
-// const arr = ["Hello", "World"];
-// for (const i of arr) {
-//     console.log(arr);
-// }
+//for of
+const arr = ["Hello", "World"];
+for (const i of arr) {
+    console.log(arr);
+}
 
 //foreach
-// const arr = ["Hello", "World"];
-// const arr2 = arr.forEach((v, i) => {
-//     console.log(v);
-// })
+const arr = ["Hello", "World"];
+const arr2 = arr.forEach((v, i) => {
+    console.log(v);
+})
 
 //while
-// const arr = ["hello", "world"];
-// let a = 0
-// while (arr.length === 2); {
-//     if (b === 5) {
-//         arr.push("java");
-//     } else {
-//         console.log(arr.join(" "));
-//     }
-//     b++;
-// }
+const arr = ["hello", "world"];
+let a = 0
+while (arr.length === 2); {
+    if (b === 5) {
+        arr.push("java");
+    } else {
+        console.log(arr.join(" "));
+    }
+    b++;
+}
